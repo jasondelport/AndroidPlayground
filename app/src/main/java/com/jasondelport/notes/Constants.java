79b01cf8246e7a50dff22bda80f2881fdacb0fdb
Jason Delport <jason.delport@keytree.co.uk>
2015-04-08 16:00:51 +0100

    Added readme.

--- app/src/main/java/com/jasondelport/notes/Constants.java
+++ app/src/main/java/com/jasondelport/notes/Constants.java
@@ -4,6 +4,8 @@ package com.jasondelport.notes;
  * Created by jasondelport on 22/02/2015.
  */
 public abstract class Constants {
+
     public static final String MAIN_FRAGMENT_TAG = "MainFragment";
     public static final String PREFERENCES = "JasonDelportPreferences";
+
 }
