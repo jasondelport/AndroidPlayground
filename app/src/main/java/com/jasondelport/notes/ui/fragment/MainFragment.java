35294a953aefec0385c17a0d2baee5f9d0bee614
Jason Delport <jason.delport@gmail.com>
2015-07-16 20:50:11 +0100

    Update MainFragment.java

--- app/src/main/java/com/jasondelport/notes/ui/fragment/MainFragment.java
+++ app/src/main/java/com/jasondelport/notes/ui/fragment/MainFragment.java
@@ -62,6 +62,19 @@ public class MainFragment extends BaseFragment {
     }
     */
 
+    /*
+public void setRetainInstance (boolean retain)
+
+Control whether a fragment instance is retained across Activity re-creation (such as from a configuration change). This can only be used with fragments not in the back stack. If set, the fragment lifecycle will be slightly different when an activity is recreated:
+
+onDestroy() will not be called (but onDetach() still will be, because the fragment is being detached from its current activity).
+onCreate(Bundle) will not be called since the fragment is not being re-created.
+onAttach(Activity) and onActivityCreated(Bundle) will still be called.
+
+
+
+    */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
