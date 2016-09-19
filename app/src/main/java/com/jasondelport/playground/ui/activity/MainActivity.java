bb9e9cc9b6788f9ba9f01a309a450b67e725afb5
Jason Delport <jason.delport@gmail.com>
2016-09-19 13:13:18 +0100

    Kiosk mode experiments.

--- app/src/main/java/com/jasondelport/playground/ui/activity/MainActivity.java
+++ app/src/main/java/com/jasondelport/playground/ui/activity/MainActivity.java
@@ -96,6 +96,35 @@ public class MainActivity extends BaseActivity implements MainFragment.OnEventLi
         super.onCreate(savedInstanceState);
         lifecycle("onCreate");
 
+        /*
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            Timber.w("hello there");
+                            decorView.setSystemUiVisibility(uiOptions);
+                        }
+                    }
+                });
+
+        */
+
         if (BuildConfig.DEBUG) {
             DebugUtils.riseAndShine(this);
         }
@@ -245,7 +274,7 @@ public class MainActivity extends BaseActivity implements MainFragment.OnEventLi
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         Timber.d("onKeyDown");
         if (keyCode == KeyEvent.KEYCODE_MENU) {
             Timber.d("MENU pressed?");
@@ -269,7 +298,6 @@ public class MainActivity extends BaseActivity implements MainFragment.OnEventLi
 
         return super.dispatchKeyEvent(event);
     }
-
 
 
 }
--- app/src/main/java/com/jasondelport/playground/ui/fragment/MainFragment.java
+++ app/src/main/java/com/jasondelport/playground/ui/fragment/MainFragment.java
@@ -177,6 +177,35 @@ public class MainFragment extends BaseFragment {
         NavUtils.openActivity(getActivity(), SensorActivity.class);
     }
 
+    @OnClick(R.id.main_button_hide_nav)
+    void hideNav() {
+        View decorView = getActivity().getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @OnClick(R.id.main_button_show_nav)
+    void showNav() {
+        View decorView = getActivity().getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
     @OnClick(R.id.main_button_detectedactivity)
     void loadDetectedActivity() {
         NavUtils.openActivity(getActivity(), DetectedActivityActivity.class);
